221f891191ab370c2e576e4cc90c99d59fd80c73
Stefan <oumansour.stefan@gmail.com>
2025-05-25 03:21:17 +0200

    Update shader.cpp

--- Source/Graphics/Metal/shader.cpp
+++ Source/Graphics/Metal/shader.cpp
@@ -7,4 +7,19 @@ bool IsNull(GfxShader *shader)
 
 GfxShader GfxLoadShader(String name, String source_code, GfxPipelineStage stage, Slice<GfxPipelineBinding> bindings) { return {}; }
 GfxShader GfxLoadShader(String name, String source_code, GfxPipelineStage stage) { return {}; }
-void GfxDestroyShader(GfxShader *shader) {}
+
+void GfxDestroyShader(GfxShader *shader)
+{
+    shader->library->release();
+    shader->function->release();
+
+    foreach (i, shader->bindings)
+    {
+        Free(shader->bindings[i].name.data, heap);
+        Free(shader->bindings[i].associated_texture_units.data, heap);
+    }
+
+    Free(shader->bindings.data, heap);
+
+    *shader = {};
+}
